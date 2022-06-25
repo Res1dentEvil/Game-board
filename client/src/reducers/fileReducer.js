5f5d571f5d141d9f8b96e258a7ee350106ea85ef
Res1dentEvil <davidenkovano@gmail.com>
2022-06-25 23:32:52 +0300

    feat: add redux

---
+++ client/src/reducers/fileReducer.js
@@ -0,0 +1,11 @@
+import { bindActionCreators } from "redux";
+
+const defaultState = {
+
+}
+export default function fileReducer(state = defaultState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+import { bindActionCreators } from "redux";
+
+const defaultState = {
+    currentUser: {},
+    isAuth: false
+}
+
+export default function userReducer(state = defaultState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
